fcb3ed71a2c4844c7d8c601e01ae191956fee2af
Anders Hoff <inconvergent@gmail.com>
2024-10-19 22:33:36 +0200

    .

--- make-code.lisp
+++ make-code.lisp
@@ -11,39 +11,15 @@
 ; (ql:quickload :evl) ; must be available locally. eg. in ~/common-lisp/evl
 ; (in-package :evl)
 
-; xprt prints the expression and the output
-
-(veq:fvdef compile-agent (a*)
-  (labels ((wrap-pfx () `(veq:fvprogn
-                           (lambda (a)
-                             (declare (agent a))
-                             (veq:xlet ((f!x (agent-x a)) (f!y (agent-y a))
-                                        (f!vx (agent-vx a)) (f!vy (agent-vy a)))
-                               (f2.@trunc ,(agent-pfx a*))))))
-           (wrap-vfx () `(veq:fvprogn
-                           (lambda (all-pos all-vel a)
-                             (declare (veq:fvec all-pos all-vel) (agent a))
-                             (veq:xlet ((f!x (agent-x a)) (f!y (agent-y a))
-                                        (f!vx (agent-vx a)) (f!vy (agent-vy a)))
-                              (f2.@trunc ,(agent-vfx a*) 100f0))))))
-    (setf (agent-pfxx a*) (eval (wrap-pfx))
-          (agent-vfxx a*) (eval (wrap-vfx))))
-  a*)
-
-(veq:fvdef make-agents (n)
-  (21%@$init (f2!@$+ (veq:f2$polygon n 50f0) (?@ (f2!@+ *m* *m* (rnd:2in-circ 10f0))))
-             ((x y) (compile-agent (m@-make-agent x y
-                                      (rnd:2in-circ 1f0)
-                                      (init-pfx) (rnd-code *exprs* '!f2))))))
 
 (veq:fvdef main (fn)
   (veq:vpr (evl:evl*
-    ; (print (veq:replace-varg (veq:proc-vv (rnd-code *exprs* '!f2))))
+    (print (veq:replace-varg (veq:proc-vv (rnd-code *exprs* '!f2))))
     ; '(~ 1 2 (~ 3 4 5 6))
     ; '(~ 1 2 (~ 3 4 5))
     ; '(values (+ 1 2) -2 -3)
     ; '(~ (+ 1 2) -2 -3)
-    '(~ (+ 1 2) -2 (~ 899 99))
+    ; '(~ (+ 1 2) -2 (~ 899 99))
     ))
 
            )
